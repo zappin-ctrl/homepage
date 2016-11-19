d09a2fac363027f9740737870b7a7ea2ec176e1e
TTtie <tttiett@outlook.com>
2016-11-19 17:49:12 +0100

    back to custom alex's backgrounds

--- backgrounds.js
+++ backgrounds.js
@@ -3,10 +3,11 @@ function randomBackground() {
       Change the number before 1 with the amount of backgrounds you have
       Files must be .jpg and names background[NUMBER].jpg
     */
-    /*
+    
     var randomNumber = Math.floor(Math.random() * 8) + 1;
     var htmlElement = document.getElementById("main");
     var style = htmlElement.style;
+    style.backgroundImage = "url(\"/images/background"+randomNumber+".jpg\")"/*
     style.backgroundColor = "transparent";
     style.backgroundImage = "url(\"https://source.unsplash.com/random/1920x1080\")";
     style.backgroundRepeat = "no-repeat";
