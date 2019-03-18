63aa15f99b2b96248ed5e9834f0061cea52a26f5
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2019-03-18 16:42:06 +0100

    More weather details

--- assets/js/pluginScript.js
+++ assets/js/pluginScript.js
@@ -115,17 +115,48 @@ document.addEventListener('DOMContentLoaded', function() {
     if (items.wkey) {
       navigator.geolocation.getCurrentPosition(function(position) {
         pos = position.coords;
-        http(`https://api.openweathermap.org/data/2.5/weather?lat=${pos.latitude}&lon=${pos.longitude}&lang=${items.wlang}&APPID=${items.wkey}`, function(r) {
-          document.getElementById('wicon').src = wicons[r.weather[0].icon]
-          document.getElementById('wname').innerText = r.name
-          document.getElementById('wdescription').innerText = r.weather[0].description.replace(/^\w/, c => c.toUpperCase());
-          if (items.temp == false) {
-            document.getElementById('wtemp').innerText = `${Math.round(parseInt(r.main.temp) * (9 / 5) - 459.67)} °F`
+        http(`https://api.openweathermap.org/data/2.5/forecast?lat=${pos.latitude}&lon=${pos.longitude}&lang=${items.wlang}&APPID=${items.wkey}`, function(r) {
+          document.getElementById('wicon').src = wicons[r.list[0].weather[0].icon]
+          document.getElementById('wname').innerText = r.city.name
+          document.getElementById('wdescription').innerText = r.list[0].weather[0].description.replace(/^\w/, c => c.toUpperCase());
+          if (items.tempc == false) {
+            document.getElementById('wtemp').innerText = `${Math.round(parseInt(r.list[0].main.temp) * (9 / 5) - 459.67)} °F`
           } else {
-            document.getElementById('wtemp').innerText = `${Math.round(parseInt(r.main.temp) - 273.15)} °C`
+            document.getElementById('wtemp').innerText = `${Math.round(parseInt(r.list[0].main.temp) - 273.15)} °C`
           }
 
           document.getElementById('wcontainer').style.display = "block";
+
+          // Generate new element for each new future time weather
+          var wLater = document.getElementById('wtime-container');
+          function newWeatherTime(data) {
+            var element = document.createElement("div");
+            var wlIcon = document.createElement("img");
+            var wlText = document.createElement("p");
+            element.className = "wdiff";
+            wlText.className = "text";
+            wlIcon.className = "icon";
+
+            if (items.tempc == false) {
+              var getTemp = `${Math.round(parseInt(data.main.temp) * (9 / 5) - 459.67)} °F`
+            } else {
+              var getTemp = `${Math.round(parseInt(data.main.temp) - 273.15)} °C`
+            }
+
+            wlIcon.src = wicons[data.weather[0].icon];
+            var getTime = moment.unix(data.dt).format("LT")
+            var getDesc = data.weather[0].description.replace(/^\w/, c => c.toUpperCase())
+            wlText.innerText = `${getTime} - ${getDesc} | ${getTemp}`;
+
+            element.appendChild(wlIcon);
+            element.appendChild(wlText);
+            wLater.appendChild(element);
+          }
+
+          // Make all the other time differences
+          for (i = 1; i < 5; i++) {
+            newWeatherTime(r.list[i]);
+          }
         })
 
       });
